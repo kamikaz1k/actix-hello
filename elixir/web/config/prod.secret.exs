98295ed44279681fda0e8cc8ee186172875c17cc
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2020-05-14 22:15:33 -0400

    added elixir/phoenix files

---
+++ elixir/web/config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :web, Web.Endpoint,
+  secret_key_base: "zIWYBxACevNFRVc7syBKkvntM8j/G1n2K/vvWnNDZcoThcs9X/oW2iUU4vWvfta/"
